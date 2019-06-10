861c4d467186ebf38a4c3ba45c6da65646c681c5
Hermes <hermeslisoma@gmail.com>
2019-06-09 21:28:15 -0400

    revert

--- express-api-app/src/serviceWorker.ts
+++ express-api-app/src/serviceWorker.ts
@@ -10,26 +10,26 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-// const isLocalhost = Boolean(
-//   window.location.hostname === 'localhost' ||
-//     // [::1] is the IPv6 localhost address.
-//     window.location.hostname === '[::1]' ||
-//     // 127.0.0.1/8 is considered localhost for IPv4.
-//     window.location.hostname.match(
-//       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-//     )
-// );
-
 const isLocalhost = Boolean(
-  window.location.hostname === 'http://hermes-react.s3-website.us-east-2.amazonaws.com' 
-  //||
+  window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    // window.location.hostname === '[::1]' ||
-    // // 127.0.0.1/8 is considered localhost for IPv4.
-    // window.location.hostname.match(
-    //   /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    // )
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
 );
+
+// const isLocalhost = Boolean(
+//   window.location.hostname === 'http://hermes-react.s3-website.us-east-2.amazonaws.com' 
+//   //||
+//     // [::1] is the IPv6 localhost address.
+//     // window.location.hostname === '[::1]' ||
+//     // // 127.0.0.1/8 is considered localhost for IPv4.
+//     // window.location.hostname.match(
+//     //   /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+//     // )
+// );
 
 type Config = {
   onSuccess?: (registration: ServiceWorkerRegistration) => void;
